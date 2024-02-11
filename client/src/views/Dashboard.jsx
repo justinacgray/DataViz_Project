0000c24970958d9da1d7a1a7cb89b5dc79d28775
Justina Gray <justinacgray@gmail.com>
2024-02-10 20:27:47 -0500

    working on token config

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -7,20 +7,29 @@ import CsrfToken from '../components/CsrfToken';
 const Dashboard = () => {
   const token = CsrfToken('csrftoken');
 
-  console.log('CSRF Token:', CsrfToken);
+
+  console.log('CSRF Token:', token);
   
   const [spreadsheet, setSpreadsheet] = useState('');
 
   const submitForm = (e) => {
     e.preventDefault();
-    axios.post(`http://localhost:8000/api/dash`)
+    axios.post(`http://localhost:8000/api/dash`, {
+      credentials: 'include',
+      method: 'POST',
+      mode: 'same-origin',
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFToken': token
+    },
+    body: {}
+    })
     .then((response)=>{
       console.log("RESPONSE---->", response)
     }).catch((error)=>{
       console.log(error)
     }, [])
-
-
 
   }
 
@@ -34,10 +43,13 @@ const Dashboard = () => {
           <h2>Upload a Spreadsheet</h2>
         </section>
           <input name="spreadsheet" type="text" placeholder="enter spreadsheet" onChange={(e) => setSpreadsheet(e.target.value)} />
+          <input type="hidden" name="csrfmiddlewaretoken" value={token} />
         <section className="">
           <input className="btn btn-primary" type="submit" value="Enter!" />
         </section>
       </form>
+      <p> ---------------------- </p>
+   
     </div>
   )
 }
--- server/data_viz_app/views.py
+++ server/data_viz_app/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 def home(request):
     return JsonResponse({'message': 'Shalom Todos!'})
 
+
+@ensure_csrf_cookie
 def dash(request):
     print("Request---------->", request)
     # return JsonResponse({})
