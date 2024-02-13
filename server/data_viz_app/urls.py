affdc804ffe62cfd81a18d8eef0b3e699022a4cb
Justina Gray <justinacgray@gmail.com>
2024-02-12 21:24:20 -0500

    django file upload started - not tested

--- server/data_viz_app/urls.py
+++ server/data_viz_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,3 +9,9 @@ urlpatterns = [
     path("dash/", views.dash, name="dash"),
     path("get_csrf/", views.get_csrf, name="get_csrf")
 ]
+
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.DATASET_URL, document_root=settings.DATASET_ROOT)
