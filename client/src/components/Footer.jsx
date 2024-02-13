b534bf0f5f65908c24559b202e240813bb674df6
Justina Gray <justinacgray@gmail.com>
2024-02-12 19:48:43 -0500

    fixed className in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer class="fixed bg-gray-700 shadow dark:bg-gray-700 bottom-0 w-full">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Copyright. All Rights Reserved.
+        <footer className="fixed bg-gray-700 shadow dark:bg-gray-700 bottom-0 w-full">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Copyright. All Rights Reserved.
                 </span>
             </div>
         </footer>
