ba4abdefbae9d95564976ee66ed1acd9de64dca0
illustrae <traehugh.dev@gmail.com>
2024-02-26 23:04:38 -0500

    removed preline and just created the component

--- client/src/components/AllCsvs.jsx
+++ client/src/components/AllCsvs.jsx
@@ -19,7 +19,7 @@ const AllCsvs = () => {
         }
         axios.get(url, config)
             .then((res) => {
-                console.log("res--->", res.data)
+                console.log("RESSS DATAAA--->", res.data.csvs)
                 setCsvList(res.data.csvs)
             })
             .catch((err) => {
@@ -27,26 +27,27 @@ const AllCsvs = () => {
             })
     }, [])
 
-    console.log("csvList", csvList)
     return (
         <div>
-            <div className="hs-dropdown relative inline-flex [--trigger:hover]">
-                <button id="hs-dropdown-hover-event" type="button" className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                    Choose a dataframe
-                    <svg className="hs-dropdown-open:rotate-180 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6" /></svg>
+            <div className="dropdown inline-block relative">
+                <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+                    <span className="mr-1">Uploaded CSV Files</span>
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
                 </button>
-
-                <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" aria-labelledby="hs-dropdown-hover-event">
-
+                <ul className="dropdown-menu absolute hidden text-gray-100 pt-1">
+                    <li clasName="">
                         {csvList.map((csv, idx) => (
-                            <a key={idx} href="#" className="flex items-center gap-x-3.5 py-1 px-2 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700">{csv.file_name}</a>
+                            <a key={idx} href="#" className="rounded-t bg-gray-700 hover:bg-gray-500 py-2 px-4 block whitespace-no-wrap">{csv.file_name}</a>
                         ))}
-        
-                </div>
+                    </li>
+                </ul>
             </div>
 
-            
         </div>
-            )
-} 
+
+
+
+
+    )
+}
 export default AllCsvs
